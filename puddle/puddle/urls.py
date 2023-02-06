aef2db66b7e047b98b2a71b8c4a60fff5dd5e69c
fxop0218 <francescoli0218@gmail.com>
2023-02-06 19:03:03 +0100

    Show images if this exists

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Views
 from core.views import index, contact
@@ -8,4 +10,4 @@ urlpatterns = [
     path("", index, name="index"),
     path("contact/", contact, name="contact"),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
