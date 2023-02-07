fdc9c9462524d928e8f7b498a9fc7f760deb7cd4
fxop0218 <francescoli0218@gmail.com>
2023-02-07 18:37:46 +0100

    Description item added

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Items
 
 # Create your views here.
+def detail(request, pk):
+    item = get_object_or_404(Items, pk=pk)
+
+    return render(request, "item/detail.html", {"item": item})
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path("", index, name="index"),
     path("contact/", contact, name="contact"),
     path("admin/", admin.site.urls),
+    path("items/", include("item.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
